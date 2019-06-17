ae3f140ba47ad1de3f798f0e4871cd3de5fa8fb2
hityou <hityou_on@hotmail.com>
2019-06-17 22:09:46 +0800

    load xml.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
-
+using System.Xml;
 namespace ConsoleApp1
 {
+    class Format
+    {
+        public string path = "format.xml";
+    }
     class Program
     {
+        string dataXml = "data.xml";
         static void Main(string[] args)
         {
+            Format f = new Format();
+            XmlDocument doc = new XmlDocument();
+            doc.Load(f.path);
+            XmlElement data = doc.DocumentElement;
+            
             Console.WriteLine("Hello World!");
         }
     }
